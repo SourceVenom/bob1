c96c50985a64599d67052eb20c1bbd9aa5d2a6d1
SourceVenom <161086308+SourceVenom@users.noreply.github.com>
2024-02-27 23:41:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BARALPOP")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Veno_m_1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
