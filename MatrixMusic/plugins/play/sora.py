da0548c57e3c579396616f9a9a13fd60d135936e
SourceVenom <161086308+SourceVenom@users.noreply.github.com>
2024-03-07 17:02:04 +0200

    Add files via upload

---
+++ MatrixMusic/plugins/play/sora.py
@@ -0,0 +1,22 @@
+import asyncio
+from pyrogram import Client, filters
+from strings.filters import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from MatrixMusic import app
+import random
+    
+
+@app.on_message(command([f"صوره", "صورة", "صور"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(2,50)
+    url = f"https://t.me/vnnkli/{rl}"
+    await client.send_photo(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الصوره لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
