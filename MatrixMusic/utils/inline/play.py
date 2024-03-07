534737a97c6f6f65caf12457113dca3ee80d1f7f
SourceVenom <161086308+SourceVenom@users.noreply.github.com>
2024-03-08 01:13:59 +0200

    Add files via upload

--- MatrixMusic/utils/inline/play.py
+++ MatrixMusic/utils/inline/play.py
@@ -1,11 +1,15 @@
 import math
 
+import config
+from AFROTOMusic import app 
+
 from pyrogram.types import InlineKeyboardButton
 
-from MatrixMusic.utils.formatters import time_to_seconds
+from AFROTOMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
+   
     buttons = [
         [
             InlineKeyboardButton(
@@ -17,7 +21,6 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
             ),
         ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/K_o_c_3")],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
@@ -34,41 +37,42 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "⦷─────────"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "─⦷────────"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "──⦷───────"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "───⦷──────"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "────⦷─────"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "─────⦷────"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "──────⦷───"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "───────⦷──"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "────────⦷─"
+        bar = "————————◉—"
     else:
-        bar = "─────────⦷"
+        bar = "—————————◉"
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
+                [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-        ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/K_o_c_3")],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ],[
+            InlineKeyboardButton(text="ᯓ 𝚁𝙴𝚂𝚞𝙼𝙴 ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="ᯓ 𝙿𝙰𝚞𝚂𝙴 ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="ᯓ 𝚁𝙴𝙿𝙻𝙰𝚈 ", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="ᯓ 𝚂𝙺𝙸𝙿 ‌", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="ᯓ 𝚂𝚃𝙾𝙿 ", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],[    
+           InlineKeyboardButton(text="𓏺𝗦𝗼𝘂𝗿𝗰𝗲ᯓ𝗩𝗘𝗡𝗢𝗠𖠛.", user_id=config.OWNER_ID),
+        ],[
+            InlineKeyboardButton(text="𖣂 اضف البوت الي مجموعتك او قناتك 𖣂", url=f"https://t.me/{app.username}?startgroup=true")],
     ]
     return buttons
 
@@ -76,14 +80,16 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/K_o_c_3")],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+            InlineKeyboardButton(text="ᯓ 𝚁𝙴𝚂𝚞𝙼𝙴  ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="ᯓ 𝙿𝙰𝚞𝚂𝙴 ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="ᯓ 𝚁𝙴𝙿𝙻𝙰𝚈 ", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="ᯓ 𝚂𝙺𝙸𝙿 ‌", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="ᯓ 𝚂𝚃𝙾𝙿 ", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],[
+          InlineKeyboardButton(text="𓏺𝗦𝗼𝘂𝗿𝗰𝗲ᯓ𝗩𝗘𝗡𝗢𝗠𖠛.", user_id=config.OWNER_ID),
+        ],[
+            InlineKeyboardButton(text="𖣂 اضف البوت الي مجموعتك او قناتك 𖣂", url=f"https://t.me/{app.username}?startgroup=true")],
     ]
     return buttons
 
@@ -93,14 +99,13 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"Playlists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"Playlists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/K_o_c_3")],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
@@ -119,7 +124,6 @@ def livestream_markup(_, videoid, user_id, mode, channel, fplay):
                 callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
             ),
         ],
-        [InlineKeyboardButton(text=_["MATRIX_BUTTON"], url=f"https://t.me/K_o_c_3")],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
@@ -153,10 +157,9 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
                 callback_data=f"forceclose {query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="▷",
+                text="𖣂 𝒓𝒆𝒔𝒖𝒎𝒆 𖣂",
                 callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
             ),
         ],
     ]
     return buttons
-    
