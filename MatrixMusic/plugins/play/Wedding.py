58d9cf5c6b03b32b7f6b9b9d746605089c99815c
SourceVenom <161086308+SourceVenom@users.noreply.github.com>
2024-03-08 23:33:23 +0200

    Update Wedding.py

--- MatrixMusic/plugins/play/Wedding.py
+++ MatrixMusic/plugins/play/Wedding.py
@@ -9,7 +9,7 @@ import random
 @app.on_message(command([f"ايه", "ايه قرانيه", "قران", "{BOT_USERNAME} ايه"]))
 async def ihd(client: Client, message: Message):
     rl = random.randint(8,20)
-    url = f"https://t.me/jbnm9*/{rl}"
+    url = f"https://t.me/jbnm9/{rl}"
     await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الايه لـك",
     reply_markup=InlineKeyboardMarkup(
             [
