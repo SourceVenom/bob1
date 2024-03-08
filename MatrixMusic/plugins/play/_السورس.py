f1bf2e11ef289d32206e5ffac7caa22eab0df2d4
SourceVenom <161086308+SourceVenom@users.noreply.github.com>
2024-03-08 09:14:31 +0200

    Update _السورس.py

--- MatrixMusic/plugins/play/_السورس.py
+++ MatrixMusic/plugins/play/_السورس.py
@@ -18,7 +18,7 @@ from random import  choice, randint
 )
 async def huhh(client: Client, message: Message):
     await message.reply_video(
-        video=f"https://telegra.ph/file/0913f5246d0532e170e21.mp4",
+        video=f"https://i.top4top.io/m_2988j1ogc0.mp4",
         caption=f"""
 ⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼""",
         reply_markup=InlineKeyboardMarkup(
